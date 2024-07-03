11b20f3bd9babc921f3b6373d5ce74e398261fd4
donghyunkim <ghffu405@gmail.com>
2024-07-03 22:53:32 +0900

    feat: 커스텀 구분자의 합 구현

--- nextstep.chap1/src/main/java/co/nextstep/chap1/StringCalculator.java
+++ nextstep.chap1/src/main/java/co/nextstep/chap1/StringCalculator.java
@@ -4,29 +4,43 @@ import java.util.List;
 
 public class StringCalculator {
 
-	private final Splitter splitter;
 	private static final int ZERO = 0;
 
+	private final Splitter splitter;
+	private final RegexSplitPattern regexSplitPattern;
 
-	public StringCalculator(Splitter splitter) {
+	public StringCalculator(Splitter splitter, RegexSplitPattern regexSplitPattern) {
 		this.splitter = splitter;
+		this.regexSplitPattern = regexSplitPattern;
 	}
 
 	public int add(String target) {
-		// if()
-		if(isEmptyOrSpace(target)) {
+		if (isEmptyOrSpace(target)) {
 			return ZERO;
 		}
-		List<Integer> numbers = splitter.splitByDefaultDelimiter(target);
+		List<Integer> numbers;
+		if (regexSplitPattern.isValid(target)) {
+			numbers = getNumbersWithCustomDelimiter(target);
+		} else {
+			numbers = getNumbersWithDefaultDelimiter(target);
+		}
 		validateNegative(numbers);
 		return sum(numbers);
+	}
 
+	private List<Integer> getNumbersWithDefaultDelimiter(String target) {
+		return splitter.splitByDefaultDelimiter(target);
+	}
+
+	private List<Integer> getNumbersWithCustomDelimiter(String target) {
+		String delimiter = regexSplitPattern.getDelimiter(target);
+		String numbersWithDelimiter = regexSplitPattern.getNumbersWithDelimiter(target);
+		return splitter.splitByDelimiter(delimiter, numbersWithDelimiter);
 	}
 
 	private static void validateNegative(List<Integer> numbers) {
-		boolean existsNegative = numbers.stream()
-			.anyMatch(number -> number < ZERO);
-		if(existsNegative) {
+		boolean existsNegative = numbers.stream().anyMatch(number -> number < ZERO);
+		if (existsNegative) {
 			throw new RuntimeException();
 		}
 	}
--- nextstep.chap1/src/test/java/co/nextstep/chap1/StringCalculatorTest.java
+++ nextstep.chap1/src/test/java/co/nextstep/chap1/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package co.nextstep.chap1;
 
+import static co.nextstep.chap1.RegexSplitPattern.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.stream.Stream;
@@ -14,7 +15,7 @@ public class StringCalculatorTest {
 	@ParameterizedTest
 	@MethodSource("provideStringAndExpected")
 	void 숫자의_합을_구한다(String target, int expected) {
-		StringCalculator stringCalculator = new StringCalculator(new Splitter());
+		StringCalculator stringCalculator = new StringCalculator(new Splitter(), CUSTOM_DELIMITER);
 		int result = stringCalculator.add(target);
 		assertThat(result).isEqualTo(expected);
 	}
@@ -36,28 +37,28 @@ public class StringCalculatorTest {
 
 	@Test
 	void 공백의_합은_0이다() {
-		StringCalculator stringCalculator = new StringCalculator(new Splitter());
+		StringCalculator stringCalculator = new StringCalculator(new Splitter(), CUSTOM_DELIMITER);
 		int result = stringCalculator.add(" ");
 		assertThat(result).isEqualTo(0);
 	}
 
 	@Test
 	void 빈문자열의_합은_0이다() {
-		StringCalculator stringCalculator = new StringCalculator(new Splitter());
+		StringCalculator stringCalculator = new StringCalculator(new Splitter(), CUSTOM_DELIMITER);
 		int result = stringCalculator.add("");
 		assertThat(result).isEqualTo(0);
 	}
 
 	@Test
 	void 음수는_더할수없다() {
-		StringCalculator stringCalculator = new StringCalculator(new Splitter());
+		StringCalculator stringCalculator = new StringCalculator(new Splitter(), CUSTOM_DELIMITER);
 		assertThatThrownBy(() -> stringCalculator.add("1,-1"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
 	void 커스텀_구분자를_포함한_숫자를_더한다() {
-		StringCalculator stringCalculator = new StringCalculator(new Splitter());
+		StringCalculator stringCalculator = new StringCalculator(new Splitter(), CUSTOM_DELIMITER);
 		int result = stringCalculator.add("//;\n1;2;3");
 		assertThat(result).isEqualTo(6);
 	}
