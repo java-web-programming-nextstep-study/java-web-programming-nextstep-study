7d50eac7e12e9725951c712e7482ead06195e828
donghyunkim <ghffu405@gmail.com>
2024-07-03 18:24:35 +0900

    feat: 쉼표 또는 콜론으로 문자열을 분리한다

---
+++ nextstep.chap1/src/test/java/co/nextstep/chap1/SplitterTest.java
@@ -0,0 +1,20 @@
+package co.nextstep.chap1;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SplitterTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = {"1,2", "1:2"})
+	void 쉼표_또는_콜론으로_문자열을_분리한다(String target) {
+		Splitter splitter = new Splitter();
+		List<Integer> splited = splitter.splitByDefaultDelimiter(target);
+		assertThat(splited).containsExactly(1, 2);
+	}
+}
