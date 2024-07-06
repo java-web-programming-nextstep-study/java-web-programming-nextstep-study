1433c491639d3d3d12cc17b0c8b10b7e1ed100cc
daeho kang <eoghdhdls@naver.com>
2024-07-06 19:39:23 +0900

    feat: 기본세팅 및 문자열 계산기 디렉토리 세팅

---
+++ kangdaeho/src/main/java/org/example/CalculatorTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator cal;
+    @Before
+    public void setup(){
+        cal = new Calculator();
+        System.out.println("Before");
+    }
+
+    @Test
+    public void add(){
+        assertEquals(9, cal.add(6, 3));
+        System.out.println("add");
+    }
+    
+    @Test
+    public void subtract(){
+        assertEquals(3, cal.subtract(6, 3));
+        System.out.println("subtract");
+    }
+
+    @After
+    public void teardown(){
+        System.out.println("teardown");
+    }
+
+}
