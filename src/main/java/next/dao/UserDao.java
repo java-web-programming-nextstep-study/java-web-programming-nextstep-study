6cf72a336f91d38b08ec45523271bb52017baa0f
kimdonghyun <ghffu405@gmail.com>
2024-09-13 14:45:09 +0900

    feat: 회원 전체 목록 조회 기능 구현

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -41,7 +41,33 @@ public class UserDao {
 
     public List<User> findAll() throws SQLException {
         // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT * FROM USERS";
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            List<User> users = new ArrayList<>();
+
+            while (rs.next()) {
+                users.add(new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email")));
+            }
+
+            return users;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     public User findByUserId(String userId) throws SQLException {
